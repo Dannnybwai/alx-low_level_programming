1e7d9d3a96f3de62ba7d36e4fe8eed7e84706076
Dannnybwai <danielgajere@gmail.com>
2022-09-08 16:32:02 -0700

    Getting difficult

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
- * main- Serves as the entry point for the program
+ * main- Entry point for the program.
  *
- * Return:By default returns zero if no errors otherwise returns non-zero value.
+ * Return: Returns zero if no errors otherwise returns non-zero value.
  */
 int main(void)
 {
-    put("Programming is like building a multilingual puzzle\n");
-    return (0);
+	put("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
