a99b0b471f60011dcfef8fa92732384f5d16ba8d
Dannnybwai <danielgajere@gmail.com>
2022-09-11 16:32:20 -0700

    doing the hard things

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+/**
+ * main - main block
+ * Description: prints all numbers
+ * return: 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("%i", i);
+	}
+	putchar('\n');
+	return (0);
+}
