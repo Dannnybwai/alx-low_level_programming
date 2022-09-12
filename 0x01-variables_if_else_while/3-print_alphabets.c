941234391c238d0c5a5ca46ba0de837e41a1fbd5
Dannnybwai <danielgajere@gmail.com>
2022-09-11 19:03:38 -0700

    i am tired

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,21 +1,26 @@
 #include <stdio.h>
+#include <stdlib.h>
 /**
- * main - print alphabet in lower and uppercase
+ * main - print alphabet
  *
  * Description: using the main function
  * Return: 0
  */
 int main(void)
 {
-	char ch;
+	char c = 'a';
 
-	for (ch = 'a' ; ch <= 'z' ; ch++)
+	while (c <= 'z')
 	}
-		putchar(ch);
+		putchar(c);
+		c++;
 	{
-	for (ch = 'A' ; ch <= 'Z' ; ch++)
+	c = 'A';
+
+	while (c <= 'Z')
 	}
-		putchar(ch);
+		putchar(c);
+		c++;
 	{
 	putchar('\n');
 	return (0);
