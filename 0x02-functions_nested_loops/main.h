6e23cc3619bcfb25668305a7a0d57d27d73ab331
Dannnybwai <danielgajere@gmail.com>
2022-09-13 10:20:28 -0700

    task

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,3 +1,11 @@
-#ifndef _ MAIN_H_
-#define _ MAIN_H_
-#endif /* _MAIN_H_ */
+#ifndef _MAIN_H_
+#define _MAIN_H_
+/**
+ * All prototypes
+ * Return: 0 (success)
+ */
+int _putchar(char c);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+
+#endif
