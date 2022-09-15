12f0497799e77f8dec82724612d869d51510c58d
Dannnybwai <danielgajere@gmail.com>
2022-09-15 07:45:53 -0700

    trial

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * positive_or_negative - testing of files
+ *
+ * Return: 0
+ */
+
+void positive_or_negative(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n > 0)
+	{
+		printf("%d is positive\n", n);
+	}
+	else if (n == 0)
+	{
+		printf("%d is zero\n", n);
+	}
+	else if (n < 0)
+	{
+		printf("%d is negative\n", n);
+	}
+	return (0);
+}
